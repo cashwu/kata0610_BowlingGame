995c5a39b1ecc468a33c5eb05baef0786f815280
cash <cash@cashwu.com>
2016-06-11 00:08:05 +0800

    coding

---
+++ kata0610_BowlingGame/BowlingGame.cs
@@ -0,0 +1,80 @@
+﻿namespace kata0610_BowlingGame
+{
+    internal class BowlingGame
+    {
+        private readonly int[] _rollPins = new int[21];
+        private int _rollIndex;
+        public void Roll(int pins)
+        {
+            _rollPins[_rollIndex] = pins;
+            _rollIndex++;
+        }
+
+        public double Score()
+        {
+            var score = 0;
+
+            var frameRollIndex = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                if (IsStrike(frameRollIndex))
+                {
+                    GetStrikeScore(ref score, frameRollIndex);
+                    frameRollIndex += 1;
+                }
+                else if (IsSpare(frameRollIndex))
+                {
+                    GetSpareScore(ref score, frameRollIndex);
+                    frameRollIndex += 2;
+                }
+                else
+                {
+                    GetNormalScore(ref score, frameRollIndex);
+                    frameRollIndex += 2;
+                }
+            }
+
+            return score;
+        }
+
+        private void GetNormalScore(ref int score, int frameRollIndex)
+        {
+            score += GetNormalFrameScore(frameRollIndex);
+        }
+
+        private void GetSpareScore(ref int score, int frameRollIndex)
+        {
+            score += 10 + GetSpareBonus(frameRollIndex);
+        }
+
+        private void GetStrikeScore(ref int score, int frameRollIndex)
+        {
+            score += 10 + GetStrikeBonus(frameRollIndex);
+        }
+
+        private int GetStrikeBonus(int frameRollIndex)
+        {
+            return _rollPins[frameRollIndex + 1] + _rollPins[frameRollIndex + 2];
+        }
+
+        private bool IsStrike(int frameRollIndex)
+        {
+            return _rollPins[frameRollIndex] == 10;
+        }
+
+        private int GetSpareBonus(int frameRollIndex)
+        {
+            return _rollPins[frameRollIndex + 2];
+        }
+
+        private bool IsSpare(int frameRollIndex)
+        {
+            return GetNormalFrameScore(frameRollIndex) == 10;
+        }
+
+        private int GetNormalFrameScore(int frameRollIndex)
+        {
+            return _rollPins[frameRollIndex] + _rollPins[frameRollIndex + 1];
+        }
+    }
+}
---
+++ kata0610_BowlingGame/BowlingGameTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace kata0610_BowlingGame
+{
+    class BowlingGameTest
+    {
+        private BowlingGame _game;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _game = new BowlingGame();
+        }
+
+        [Test]
+        public void Roll_0_pins()
+        {
+            GetRolls(_game, 20, 0);
+
+            Assert.AreEqual(0, _game.Score());
+        }
+
+        [Test]
+        public void Roll_1_pins()
+        {
+            GetRolls(_game, 20, 1);
+
+            Assert.AreEqual(20, _game.Score());
+        }
+
+        [Test]
+        public void Have_1_spare()
+        {
+            _game.Roll(4);
+            _game.Roll(6);
+            _game.Roll(2);
+
+            Assert.AreEqual(14, _game.Score());
+        }
+
+        [Test]
+        public void Have_1_strike()
+        {
+            _game.Roll(10);
+            _game.Roll(3);
+            _game.Roll(1);
+
+            Assert.AreEqual(18, _game.Score());
+        }
+
+        [Test]
+        public void PerfectGame()
+        {
+            GetRolls(_game, 12, 10);
+
+            Assert.AreEqual(300, _game.Score());
+        }
+
+        private void GetRolls(BowlingGame game, int rollCount, int pins)
+        {
+            for (int i = 1; i <= rollCount; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+    }
+}
